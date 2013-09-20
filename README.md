ca512b2639cbb042408802751bf965dd70d67a45
twistedR <me@raveeshness.com>
2013-09-20 02:07:43 -0400

    added styles for the projects table\n Fixed js by removing images from the side menu

--- README.md
+++ README.md
@@ -9,3 +9,5 @@ It required the [Simple Theme Plugin](https://wiki.jenkins-ci.org/display/JENKIN
 ## USAGE
 
 Just add the master.css and master.js to the appropriate dialog boxes and enjoy a sligtly better experience :)
+
+## THIS IS STILL A WORK IN PROGRESS
--- js/master.js
+++ js/master.js
@@ -1,19 +1,7 @@
 document.observe("dom:loaded", function() {
   //Get rid of stupid extra nbsp in side panel
-  $$('#side-panel #navigation #tasks .task').each(function(c,i){
-
-
-  	// START BY FIXING THE HTML IN THE SIDE PANEL
-  	var newlink = document.createElement('a');
-  	newlink.href = c.children[0].href
-	newlink.innerHTML = c.children[0].innerHTML;
-	newlink.innerHTML += c.children[1].innerHTML;
-	c.innerHTML="";
-	c.appendChild(newlink)
-
-
-
-	//TODO: Change all the icons
-  	
+  $$('#side-panel #navigation #tasks .task a:first-child').each(function(node){
+    node.
+    node.remove();
   });
 });
