3f3b1ccefa84a53ac826120d72028daefb2d2918
twistedR <me@raveeshness.com>
2013-09-23 03:37:07 -0400

    Trying out a new style
    Changed the header.
    Added styles to make footer stick to bottom

--- js/master.js
+++ js/master.js
@@ -1,7 +1,3 @@
 document.observe("dom:loaded", function() {
-  //Get rid of stupid extra nbsp in side panel
-  $$('#side-panel #navigation #tasks .task a:first-child').each(function(node){
-    node.
-    node.remove();
-  });
+
 });
